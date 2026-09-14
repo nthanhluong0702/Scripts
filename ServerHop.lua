3769fc444fe3b0d15cab174f1bdf43fa61304d21
hnahtgnoul <bryanasaki@gmail.com>
2026-09-14 16:37:12 +0700

    Update ServerHop.lua

--- ServerHop.lua
+++ ServerHop.lua
@@ -5,40 +5,45 @@ local LocalPlayer = Players.LocalPlayer
 
 local HopModule = {}
 
-local BloxFruitsPlaces = {
-    [2753915549] = "Sea 1",
-    [4442272183] = "Sea 2",
-    [7449423635] = "Sea 3"
-}
-
 function HopModule.Hop()
     local currentPlaceId = game.PlaceId
-    
-    if not BloxFruitsPlaces[currentPlaceId] then
-        warn("Không đúng bản đồ Blox Fruits! Đang hủy Server Hop.")
-        return
-    end
-    
     local servers = {}
     local cursor = ""
-
-    local success = pcall(function()
+    
+    repeat
         local url = "https://games.roblox.com/v1/games/" .. currentPlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
-        local response = HttpService:JSONDecode(game:HttpGet(url))
+        if cursor ~= "" then
+            url = url .. "&cursor=" .. cursor
+        end
         
-        if response and response.data then
-            for _, server in ipairs(response.data) do
-                if server.playing and server.maxPlayers and server.playing < (server.maxPlayers - 2) and server.id ~= game.JobId then
+        local success, result = pcall(function()
+            return HttpService:JSONDecode(game:HttpGet(url))
+        end)
+        
+        if success and result and result.data then
+            for _, server in ipairs(result.data) do
+                if server.playing and server.maxPlayers and server.playing < (server.maxPlayers - 1) and server.id ~= game.JobId then
                     table.insert(servers, server.id)
                 end
             end
+            cursor = result.nextPageCursor or ""
+        else
+            break
         end
-    end)
-    
-    if success and #servers > 0 then 
+    until #servers >= 30 or cursor == "" 
+
+    if syn and syn.queue_on_teleport then
+        syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/nthanhluong0702/Scripts/refs/heads/main/scripts.luau"))()')
+    elseif queue_on_teleport then
+        queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/nthanhluong0702/Scripts/refs/heads/main/scripts.luau"))()')
+    end
+
+    if #servers > 0 then
         local targetServerId = servers[math.random(1, #servers)]
         
-        local tpSuccess, err = pcall(function()
+        task.wait(0.3)
+        
+        local tpSuccess = pcall(function()
             TeleportService:TeleportToPlaceInstance(currentPlaceId, targetServerId, LocalPlayer)
         end)
         
